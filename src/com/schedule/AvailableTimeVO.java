fd9237abcd36d1f51b0b9450aace6d840f6798c2
Nikhil Reddy <nikkireddy81@gmail.com>
2020-05-05 14:32:40 +0000

    first commit on RoomScheduling

---
+++ src/com/schedule/AvailableTimeVO.java
@@ -0,0 +1,28 @@
+package com.schedule;
+
+public class AvailableTimeVO {
+
+	private String startDate;
+	private String endDate;
+	private String roomNumber;
+	
+	public String getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	public String getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+	public String getRoomNumber() {
+		return roomNumber;
+	}
+	public void setRoomNumber(String roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+	
+}
