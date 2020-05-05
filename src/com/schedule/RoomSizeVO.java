cb3a5d5eee59409f9172a2aec04f1532dd8f3d7e
Nikhil Reddy <nikkireddy81@gmail.com>
2020-05-05 14:34:22 +0000

    first commit on RoomScheduling

---
+++ src/com/schedule/RoomSizeVO.java
@@ -0,0 +1,21 @@
+package com.schedule;
+
+public class RoomSizeVO {
+
+	String roomNumber;
+	Integer roomSize;
+	public String getRoomNumber() {
+		return roomNumber;
+	}
+	public void setRoomNumber(String roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+	public Integer getRoomSize() {
+		return roomSize;
+	}
+	public void setRoomSize(Integer roomSize) {
+		this.roomSize = roomSize;
+	}
+	
+	
+}
